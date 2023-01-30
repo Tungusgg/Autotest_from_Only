5d8a9c9cdf63c74e7e124b80fe371ae4befefcff
Tungusgg <112103770+Tungusgg@users.noreply.github.com>
2023-01-30 23:49:44 +0700

    Add files via upload

--- src/test/java/tech/begetTest/NiceKlass.java
+++ src/test/java/tech/begetTest/NiceKlass.java
@@ -3,24 +3,15 @@ package tech.begetTest;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.util.Random;
-
-public class NiceKlass {
+public class NiceKlass extends WebDriverSet{
     @Test //Тест успешно пройдён, производится РАСПРЕДЕЛЕНИЕ по группам
     //***Требование покрыто
     public void Nams(){
-        WebDriver driver = new ChromeDriver();
         driver.get("https://kobor.teslaserver.ru/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]/h2")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]")));
         new Actions(driver)
                 .keyDown(Keys.ESCAPE)
                 .sendKeys("a")
@@ -39,14 +30,11 @@ public class NiceKlass {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(), \"Холодильник\")]")));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(), \"Бирюса\")]")));
         System.out.println("Распределение по группам complitied");
-        driver.quit();
     }
     @Test //Тест успешно пройдён, производится поиск по НАЗВАНИЮ товара
     public void Name(){
-        WebDriver driver = new ChromeDriver();
         driver.get("https://kobor.teslaserver.ru/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]/h2")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]")));
         new Actions(driver)
                 .keyDown(Keys.ESCAPE)
                 .sendKeys("a")
@@ -65,16 +53,13 @@ public class NiceKlass {
         driver.findElement(By.xpath("//div[contains(@title, 'Стул')]")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@title, 'Стул')]")));
         System.out.println("Стул complitied");
-    driver.quit();
     }
     @Test //Тест успешен, автозамена RU/EN производится
     //Поиск производится с учетом неверно выбранной раскладки пользователя RU/EN (автозамена).
     //Требование покрыто, при вводе Cnek вместо Стул, производится автозамена, поиск выполняется верно.
     public void transit(){
-        WebDriver driver = new ChromeDriver();
         driver.get("https://kobor.teslaserver.ru/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]/h2")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]")));
         new Actions(driver)
                 .keyDown(Keys.ESCAPE)
                 .sendKeys("a")
@@ -93,17 +78,14 @@ public class NiceKlass {
         driver.findElement(By.xpath("//div[contains(@title, 'Стул')]")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@title, 'Стул')]")));
         System.out.println("Автозамена complitied");
-        driver.quit();
     }
     @Test //Тест успешно пройдён, производится поиск по АРТИКЛУ товара
     //Вывод результатов поиска производится без дублирования:
     // товар, найденный по нескольким из указанных критериев, выводится в результатах поиска один раз.
     //***Требование покрыто этим тестом, вывод одного уникального значения, товар не повторяется.
     public void articul(){
-        WebDriver driver = new ChromeDriver();
         driver.get("https://kobor.teslaserver.ru/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]/h2")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]")));
         new Actions(driver)
                 .keyDown(Keys.ESCAPE)
                 .sendKeys("a")
@@ -122,14 +104,11 @@ public class NiceKlass {
         driver.findElement(By.xpath("//*[@id=\"scrollWrapper\"]/div[4]/div[3]/div/div[1]/div/a/div[3]")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@title, 'Стул')]")));
         System.out.println("Артикул complitied");
-        driver.quit();
     }
     @Test //Тест успешно пройдён, в описании присутствует введённое нами значение.
     public void info(){
-        WebDriver driver = new ChromeDriver();
         driver.get("https://kobor.teslaserver.ru/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]/h2")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]")));
         new Actions(driver)
                 .keyDown(Keys.ESCAPE)
                 .sendKeys("a")
@@ -151,17 +130,14 @@ public class NiceKlass {
         driver.findElement(By.xpath("//*[@id=\"scrollWrapper\"]/div[5]/div[2]/div/div[1]/div[1]/div[2]/div[1]/div/div[2]")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[contains(text(), 'Стул')]")));
         System.out.println("Описание complitied");
-        driver.quit();
     }
     @Test //Тест успешно пройдён, производится поиск по НАЗВАНИЮ товара
     //Производится автоматический поиск ошибок / опечаток в поисковом запросе и их замена. Учитываются:
     //Орфографические ошибки в словах общего употребления.
     //***Требование покрыто, орфографические ошибки исправляеются на верное значение
     public void ErrorName(){
-        WebDriver driver = new ChromeDriver();
         driver.get("https://kobor.teslaserver.ru/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]/h2")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]")));
         new Actions(driver)
                 .keyDown(Keys.ESCAPE)
                 .sendKeys("a")
@@ -180,17 +156,14 @@ public class NiceKlass {
         driver.findElement(By.xpath("//div[contains(@title, 'Холодильник')]")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@title, 'Холодильник')]")));
         System.out.println("Орфография complitied");
-        driver.quit();
     }
     @Test //Тест провален, автозамена не всегда исправляется слова, которые имеют не более 3-х ошибок
     //Производится автоматический поиск ошибок / опечаток в поисковом запросе и их замена. Учитываются:
     //Опечатки в названии и артикуле товара. Автоматически исправляются слова, содержащие не более 3 ошибочных символов.
     //***Требование покрыто, не все слова исправляются имея не более 3-х ошибок. К примеру слово "Сстулл"-исправляется, а "Стуллл"-нет.
     public void ErrorName2(){
-        WebDriver driver = new ChromeDriver();
         driver.get("https://kobor.teslaserver.ru/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]/h2")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]")));
         new Actions(driver)
                 .keyDown(Keys.ESCAPE)
                 .sendKeys("a")
@@ -209,16 +182,13 @@ public class NiceKlass {
         driver.findElement(By.xpath("//div[contains(@title, 'Стул')]")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@title, 'Стул')]")));
         System.out.println("Автозамена опечаток не более 3-х ошибок complitied");
-        driver.quit();
     }
     @Test //Тест успешно пройдён, производится распознавание транслитерации
     //Производится автоматическое распознавание транслитерации и приведение поискового запроса к нативному языку.»
     //***Требование покрыто, происходит автоматическое распознавание транслитерации и приведение поискового запроса к нативному языку.
     public void TranslationName(){
-        WebDriver driver = new ChromeDriver();
         driver.get("https://kobor.teslaserver.ru/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]/h2")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[12]/div/div/div[2]")));
         new Actions(driver)
                 .keyDown(Keys.ESCAPE)
                 .sendKeys("a")
@@ -237,6 +207,5 @@ public class NiceKlass {
         driver.findElement(By.xpath("//div[contains(@title, 'Холодильник')]")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@title, 'Холодильник')]")));
         System.out.println("Транслитерация complitied");
-        driver.quit();
     }
 }
